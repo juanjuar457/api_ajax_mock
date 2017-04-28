f3880cc6ac75cc606d801a399808c16b0816bd3a
juanjuar457 <testjj4573421>
2017-04-28 09:14:24 -0700

    node stone, need test for PUT and POST, and donegit status yay

--- test/test-server.js
+++ test/test-server.js
@@ -25,6 +25,12 @@ function seedMaterialData() {
   return Material.insertMany(seedData);
 }
 
+function generateVendor() {
+	console.log('got to vendor')
+	const vendor = ["Medisca", "Letco", "B & B Pharma"]
+	return vendor[Math.floor(Math.random() * vendor.length)]; 
+}
+
 function generateQuantity(){
 	console.log('gen quantity')
 	const quantity = [
@@ -55,8 +61,7 @@ function generateUnits() {
 //to execute on the tests...
 function generateMaterialData() {
   return {
-    // vendor: faker.vendor(), //not sure whats up with the faker npm!
-    //generateVendor(),
+    vendor: generateVendor(), //not sure whats up with the faker npm!
     quantity: generateQuantity(), //make function for this  
     product_name: generateProductName(), 
     catalog_number: generateCatalogNumber(),
@@ -72,7 +77,6 @@ function tearDownDb() {
     return mongoose.connection.dropDatabase();
 }
 
-// //need open / close server return promise.
 describe('Material API resource', function() {
   // we need each of these hook functions to return a promise
   // otherwise we'd need to call a `done` callback. `runServer`,
@@ -96,29 +100,123 @@ describe('Material API resource', function() {
 
 
  // //whole thing is wrapped in a describe, like unit tests examples... 
-	describe('DELETE endpoint', function() {
-	    it('delete a material by id', function() {
-	      let material;
-	      return Material
-	        .findOne()
-	        .exec()
-	        .then(function(_material) {
-	          material = _material;
-	          return chai.request(app).delete(`/material/${material.id}`);
-	        })
-	        .then(function(res) {
-	          res.should.have.status(204);
-	          return Material.findById(Material.id).exec();
-	        })
-	        .then(function(_material) {
-	          // when a variable's value is null, chaining `should`
-	          // doesn't work. so `_restaurant.should.be.null` would raise
-	          // an error. `should.be.null(_restaurant)` is how we can
-	          // make assertions about a null value.
-	          should.not.exist(_material);
-	        })
-	    })    
-	})
+	// describe('DELETE endpoint', function() {
+	//     it('delete a material by id', function() {
+	//       let material;
+	//       return Material
+	//         .findOne()
+	//         .exec()
+	//         .then(function(_material) {
+	//           material = _material;
+	//           return chai.request(app).delete(`/materials/${material.id}`);
+	//         })
+	//         .then(function(res) {
+	//           res.should.have.status(204);
+	//           return Material.findById(Material.id).exec();
+	//         })
+	//         .then(function(_material) {
+	//           // when a variable's value is null, chaining `should`
+	//           // doesn't work. so `_restaurant.should.be.null` would raise
+	//           // an error. `should.be.null(_restaurant)` is how we can
+	//           // make assertions about a null value.
+	//           should.not.exist(_material);
+	//         })
+	//     })    
+	// })
+	//***** pick up here DON't FORGET POST!!!!! should be easy! check post in server! 
+	 describe('GET endpoint', function() {
 
-	describe()
-})
+    	it('should return all existing materials', function() {
+      // strategy:
+      //    1. get back all restaurants returned by by GET request to `/restaurants`
+      //    2. prove res has right status, data type
+      //    3. prove the number of restaurants we got back is equal to number
+      //       in db.
+      //
+      // need to have access to mutate and access `res` across
+      // `.then()` calls below, so declare it here so can modify in place
+      let res;
+      return chai.request(app)
+        .get('/materials')
+        .then(function(_res) {
+          // so subsequent .then blocks can access resp obj.
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.materials.should.have.length.of.at.least(1);
+          return Material.count();
+        })
+        .then(function(count) {
+          res.body.materials.should.have.length.of(count);
+        });
+    });
+
+
+    it('should return restaurants with right fields', function() {
+      // Strategy: Get back all restaurants, and ensure they have expected keys
+
+      let resMaterial;
+      return chai.request(app)
+        .get('/materials')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.materials.should.be.a('array');
+          res.body.materials.should.have.length.of.at.least(1);
+
+          res.body.materials.forEach(function(material) {
+            material.should.be.a('object');
+            material.should.include.keys(
+              'id', 'vendor', 'quantity', 'product_name', 'catalog_number', 'unit_size', 'units');
+          });
+          resMaterial = res.body.materials[0];
+          return Material.findById(resMaterial.id);
+        })
+        .then(function(material) {
+
+          resMaterial.id.should.equal(material.id);
+          resMaterial.vendor.should.equal(material.vendor);
+          resMaterial.quantity.should.equal(material.quantity);
+          resMaterial.product_name.should.equal(material.product_name);
+          resMaterial.catalog_number.should.equal(material.catalog_number);
+          resMaterial.unit_size.should.equal(material.unit_size);
+          resMaterial.units.should.equal(material.units);
+        });
+    });
+  });
+
+
+
+	//**************PUT TEST 	
+	//should it have update data with everything?? 
+	//we're adding onBackOrder with the dom logic function on the client... doing ajax 
+	//call inside! 
+	// describe('PUT endpoint', function() {
+	// 	it('should update fields you send over', function() {
+	// 		const updateData = {
+	// 			onBackOrder: 
+	// 		}; 
+
+	// 		return Material
+	// 		.findOne()
+	// 		.exec()
+	// 		.then(function(material) {
+	// 			updateData.id = material.id; 
+	// 			return chai.request(app)
+	// 				.put('/materials/onBackOrder') //alt below
+	// 				// .put('/materials/${material.id}')
+	// 				.send(updateData); 
+	// 		})
+	// 		.then(function(res) {
+	// 			res.should.have.status(204);
+
+	// 			return Material.findById(updateData.id).exec();			
+	// 		})
+	// 		then(function(material) {
+	// 			material.vendor.should.equal(updateData.vendor);
+	// 			material.product_name.should.equal(updateData.product_name); });
+	// 	});
+	// });
+}); //everything is wrapped in the 1st describe form the top! 
+
+
